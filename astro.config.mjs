ead81ff9aa699bf14e05230a687f2350397d2405
Dante Dev <jdap.dmc@gmail.com>
2024-11-11 16:41:18 -0400

    [FEAT]: basic ssl plugin vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import ssl from '@vitejs/plugin-basic-ssl'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://dantedev.dev',
     integrations: [
         mdx(),
         sitemap(),
         tailwind({
             applyBaseStyles: false
         })
-    ]
+    ],
+    server: {
+      https: true,
+    },
+    plugins: [ssl()]
 });
